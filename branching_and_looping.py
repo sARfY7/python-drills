36fead7917af93597ac04361f6d79fbdd83526c7
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-11-30 11:00:22 +0530

    Initial Commit

--- branching_and_looping.py
+++ branching_and_looping.py
@@ -1,15 +1,47 @@
 def integers_from_start_to_end_using_range(start, end, step):
     """return a list"""
-    pass
+    result = []
+    for i in range(start, end, step):
+        result.append(i)
+    return result
 
 
 def integers_from_start_to_end_using_while(start, end, step):
     """return a list"""
-    pass
+    result = []
+    i = start
+    if (step < 0):
+        while (i > end):
+            result.append(i)
+            i += step
+    else:
+        while (i < end):
+            result.append(i)
+            i += step
+    return result
 
 
 def two_digit_primes():
     """
     Return a list of all two-digit-primes
     """
-    pass
+    result = []
+    N = 100
+    is_prime = [1] * N
+    is_prime[0], is_prime[1] = 0, 0
+
+    i = 2
+    while ((i * i) < N):
+        if (is_prime[i] == 0):
+            i += 1
+            continue
+        j = i * 2
+        while (j < N):
+            is_prime[j] = 0
+            j += i
+        i += 1
+    for i in range(10, N):
+        if (is_prime[i] == 1):
+            result.append(i)
+
+    return result
