76265ad0896d32296d129f0584871bda07531be5
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-12-03 10:15:12 +0530

    Initial Commit

--- dictionaries.py
+++ dictionaries.py
@@ -3,14 +3,28 @@ def word_count(s):
     Find the number of occurrences of each word
     in a string(Don't consider punctuation characters)
     """
-    pass
+    punctuations = [",", "."]
+    result = {}
+    for word in s.split(" "):
+        unpunctuated_word = ""
+        for char in word:
+            if (char not in punctuations):
+                unpunctuated_word += char
+        if (unpunctuated_word in result):
+            result[unpunctuated_word] += 1
+        else:
+            result[unpunctuated_word] = 1
+    return result
 
 
 def dict_items(d):
     """
     Return a list of all the items - (key, value) pair tuples - of the dictionary, `d`
     """
-    pass
+    result = []
+    for k in d:
+        result.append((k, d[k]))
+    return result
 
 
 def dict_items_sorted(d):
@@ -18,4 +32,8 @@ def dict_items_sorted(d):
     Return a list of all the items - (key, value) pair tuples - of the dictionary, `d`
     sorted by `d`'s keys
     """
-    pass
+    result = []
+    sorted_keys = sorted(list(d.keys()))
+    for k in sorted_keys:
+        result.append((k, d[k]))
+    return result
