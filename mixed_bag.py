ac530c0dabe470c586db67f365982501806dd355
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-12-03 11:04:49 +0530

    Initial Commit

--- mixed_bag.py
+++ mixed_bag.py
@@ -1,23 +1,31 @@
+import random
+import os
+
+
 def unique(items):
     """
     Return a set of unique values belonging to the `items` list
     """
-    pass
+    return set(items)
 
 
 def shuffle(items):
     """
     Shuffle all items in a list
     """
+    random.shuffle(items)
+    return items
 
 
 def getcwd():
     """
     Get current working directory
     """
+    return os.getcwd()
 
 
 def mkdir(name):
     """
     Create a directory at the current working directory
     """
+    os.mkdir(name)
