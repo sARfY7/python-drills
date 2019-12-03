98380db5cbf32a052147144d2331676591586486
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-12-03 10:37:17 +0530

    Initial Commit

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import math
+
 """
 * Create a class called `Point` which has two instance variables,
 `x` and `y` that represent the `x` and `y` co-ordinates respectively. 
@@ -10,4 +12,11 @@ returns the distance between the two points.
 
 
 class Point:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, coordinate):
+        x_diff = coordinate.x - self.x
+        y_diff = coordinate.y - self.y
+        return math.sqrt(x_diff**2 + y_diff**2)
